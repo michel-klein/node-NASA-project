b6c235812270b487b3d76e6c528f3bc2a0f32e74
Michel Klein <michel.silva@eduzz.com>
2022-12-07 15:56:17 -0300

    feat: POST add launches

--- server/src/models/launches.model.js
+++ server/src/models/launches.model.js
@@ -7,7 +7,7 @@ const launch = {
     mission: 'Kepler Exploration X',
     rocket: 'Explorer IS1',
     launchDate: new Date('December 27, 2030'),
-    destination: 'Kepler-442 b',
+    target: 'Kepler-442 b',
     customer: ['ZTM', 'NASA'],
     upcoming: true,
     success: true,
@@ -16,7 +16,7 @@ const launch = {
 launches.set(launch.flightNumber, launch);
 
 function getLaunches() {
-    Array.from(launches.values())
+    return Array.from(launches.values())
 }
 
 function addNewLaunch(launch) {
