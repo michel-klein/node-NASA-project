85b2d762d133107065fb2d9603572374b2caf62f
Michel Klein <michel.silva@eduzz.com>
2022-12-08 11:18:45 -0300

    feat: mongoose config

--- server/src/server.js
+++ server/src/server.js
@@ -1,12 +1,31 @@
+require('dotenv').config();
 const http = require('http');
 const app = require('./app');
+const mongoose = require('mongoose');
 const { loadPlanetsData } = require('./models/planets.model');
 
 const PORT = process.env.PORT || 8000;
 
+const MONGO_URL = process.env.MONGO_URL;
+
 const server = http.createServer(app);
 
+mongoose.connection.once('open', () => {
+    console.log('MongoDB connection ready!');
+});
+  
+mongoose.connection.on('error', (err) => {
+    console.error(err);
+});   
+
 async function startServer() {
+    await mongoose.connect(MONGO_URL, {
+        useNewUrlParser: true,
+        useFindAndModify: false,
+        useCreateIndex: true,
+        useUnifiedTopology: true,
+      });
+
     await loadPlanetsData();
 
     server.listen(PORT, () => {
